d0cc2f1972fa76ea5a6418da8d59d67ca92aeee7
rq-ascaloncore-at-321899771753 <ascalon_core@gmail.com>
2026-01-10 01:48:12 +0900

    ui adjustment-2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,14 @@ class StaticPagesController < ApplicationController
     @total_inventory_value = current_user.stocks.total_inventory_value
     @total_knowledge_asset_value = current_user.stocks.total_knowledge_asset_value
     @total_impairment_loss = current_user.stocks.total_impairment_loss
+
+    # 適正在庫内訳
+    @unread_value = current_user.stocks.where(status: :unread).sum(:book_value) || 0
+    @unread_count = current_user.stocks.where(status: :unread).count
+    @reading_value = current_user.stocks.where(status: :reading).sum(:book_value) || 0
+    @reading_count = current_user.stocks.where(status: :reading).count
+
+    # 知識資産の詳細
+    @completed_count = current_user.stocks.where(status: :completed).count
   end
 end
